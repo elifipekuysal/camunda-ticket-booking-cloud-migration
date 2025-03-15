b701e8dd191758d8505ddc405d1a9214e6602637
Elif İpek Uysal <elifipekuysal@gmail.com>
2025-03-15 15:45:00 +0100

    change alb name

--- booking-service/lib/booking-service-stack.ts
+++ booking-service/lib/booking-service-stack.ts
@@ -45,7 +45,7 @@ export class BookingServiceStack extends Stack {
     });
     albSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'Allow HTTP traffic');
 
-    const alb = new elbv2.ApplicationLoadBalancer(this, 'JavaAppALB', {
+    const alb = new elbv2.ApplicationLoadBalancer(this, 'BookingServiceALB', {
       vpc,
       internetFacing: true,
       securityGroup: albSecurityGroup,
