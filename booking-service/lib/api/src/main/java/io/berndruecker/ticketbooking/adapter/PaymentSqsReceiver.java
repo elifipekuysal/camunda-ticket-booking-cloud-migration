8ab773c92300c83ef1fd980c55ab71e41526f654
Elif İpek Uysal <elifipekuysal@gmail.com>
2025-03-17 14:18:11 +0100

    remove unnecessary import statement

--- booking-service/lib/api/src/main/java/io/berndruecker/ticketbooking/adapter/PaymentSqsReceiver.java
+++ booking-service/lib/api/src/main/java/io/berndruecker/ticketbooking/adapter/PaymentSqsReceiver.java
@@ -5,7 +5,6 @@ import java.util.Collections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
