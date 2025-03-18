7ea6cd460fc53d284e8ec5968f469cd24687f579
Elif İpek Uysal <elifipekuysal@gmail.com>
2025-03-18 17:21:47 +0100

    publish message to zeebe client after payment completed

--- booking-service/lib/api/src/main/java/io/berndruecker/ticketbooking/adapter/RetrievePaymentAdapter.java
+++ booking-service/lib/api/src/main/java/io/berndruecker/ticketbooking/adapter/RetrievePaymentAdapter.java
@@ -1,14 +1,18 @@
 package io.berndruecker.ticketbooking.adapter;
 
 import io.berndruecker.ticketbooking.ProcessConstants;
+import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.client.api.response.ActivatedJob;
 import io.camunda.zeebe.spring.client.annotation.JobWorker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.util.Collections;
@@ -18,13 +22,19 @@ import java.util.UUID;
 @Component
 public class RetrievePaymentAdapter {
 
-  Logger logger = LoggerFactory.getLogger(RetrievePaymentAdapter.class);
+  private final Logger logger = LoggerFactory.getLogger(RetrievePaymentAdapter.class);
+
+  private final ZeebeClient client;
 
   @Value("${ticketbooking.paymentreceiver.lambda.endpoint}")
   private String endpoint;
 
   @Autowired
   private RestTemplate restTemplate;
+
+  public RetrievePaymentAdapter(@Qualifier("zeebeClientLifecycle") ZeebeClient client, ObjectMapper objectMapper) {
+    this.client = client;
+  }
 
   @JobWorker(type = "retrieve-payment")
   public Map<String, Object> callPaymentReceiverRestService(final ActivatedJob job) throws IOException {
@@ -39,6 +49,13 @@ public class RetrievePaymentAdapter {
       PaymentResponseMessage paymentResponse = restTemplate
           .getForObject(String.format("%s?paymentRequestId=%s", endpoint, paymentRequestId),
               PaymentResponseMessage.class);
+
+      client.newPublishMessageCommand()
+        .messageName("msg-payment-received")
+        .correlationKey(paymentResponse.paymentRequestId)
+        .variables(Collections.singletonMap("paymentConfirmationId", paymentResponse.paymentConfirmationId))
+        .send()
+        .join();
 
       logger.info("RetrievePaymentAdapter - Succeeded with " + paymentResponse.paymentConfirmationId);
 
