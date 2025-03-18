65c4c92a33237cdc7c59c66758ef3b9e00004ef1
Elif İpek Uysal <elifipekuysal@gmail.com>
2025-03-18 11:43:15 +0100

    fix module error

--- ticket-generator-lambda/webpack.config.js
+++ ticket-generator-lambda/webpack.config.js
@@ -5,10 +5,26 @@ module.exports = {
   entry: './lib/handler/index.js',
   target: 'node',
   mode: 'production',
-  externals: [nodeExternals()],
+  externals: [nodeExternals(
+    {
+      allowlist: ['mongodb', 'bson']
+    }
+  )],
   output: {
     libraryTarget: 'commonjs2',
     path: path.resolve(__dirname, './lib/dist'),
     filename: 'index.js',
   },
+  resolve: {
+    fallback: {
+      crypto: false,
+      kerberos: false,
+      '@mongodb-js/zstd': false,
+      'aws4': false,
+      'snappy': false,
+      'socks': false,
+      'gcp-metadata': false,
+      'mongodb-client-encryption': false
+    }
+  }
 };
